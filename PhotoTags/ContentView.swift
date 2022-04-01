99e8b6838b2aef19a5ef0e2ebfdb255d8c7c799e
Andres <andresr774@gmail.com>
2022-04-01 14:23:10 -0500

    Initial Commit

---
+++ PhotoTags/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoTags
+//
+//  Created by Andres camilo Raigoza misas on 1/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoTags/PhotoTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTagsApp.swift
+//  PhotoTags
+//
+//  Created by Andres camilo Raigoza misas on 1/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
